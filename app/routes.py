d257073809da9de13266888ee31918dc5f418e14
Mac <mackenziespeyton@gmail.com>
2021-10-17 17:25:47 -0700

    Registed books_bp for the app in __init__ file and added endpoints for books and book_id to routes.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,38 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Book:
+    def __init__(self, id, title, description): 
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Interview with a Vampire", "The first book in Anne Rice's Vampire Chronicles."),
+    Book(2, "The Vampire Lestat", "The second book in Anne Rice's Vampire Chronicles."), 
+    Book(3, "Queen of the Damned", "The third book in Anne Rice's Vampire Chronicles")
+]
+
+books_bp = Blueprint("books", __name__, url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def get_books(): 
+    books_response = []
+    for book in books: 
+        books_response.append({
+            "id": book.id,
+            "title": book.title, 
+            "description": book.description
+        })
+    return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def get_book(book_id): 
+    book_id = int(book_id)
+    for book in books: 
+        if book.id == book_id: 
+            return{
+                "id": book.id,
+                "title": book.title, 
+                "description": book.description
+            }
 
