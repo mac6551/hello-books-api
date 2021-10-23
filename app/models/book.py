9db3d8a0b0eeac85322d14ffa9bdd5a8960c0409
Mac <mackenziespeyton@gmail.com>
2021-10-22 17:46:46 -0700

    Create book model

---
+++ app/models/book.py
@@ -0,0 +1,6 @@
+from app import db
+
+class Book(db.Model): 
+    id = db.Column(db.Interger, primary_key = True, autoincrement = True)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
